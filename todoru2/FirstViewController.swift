145de3c465fae59bf3e53b8660871685892610fd
Tatsuya <t.mugen282@gmail.com>
2017-05-26 16:58:59 +0900

    Initial Commit

---
+++ todoru2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  todoru2
+//
+//  Created by 福島達也 on 2017/05/26.
+//  Copyright © 2017年 Tatsuya Fukushima. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

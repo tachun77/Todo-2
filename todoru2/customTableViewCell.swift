ac359f224eda989a450acf34d7c280a4864d69b6
Tatsuya <t.mugen282@gmail.com>
2017-05-28 15:21:52 +0900

    first commit

---
+++ todoru2/customTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  customTableViewCell.swift
+//  
+//
+//  Created by 福島達也 on 2017/05/27.
+//
+//
+
+import UIKit
+
+class customTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
